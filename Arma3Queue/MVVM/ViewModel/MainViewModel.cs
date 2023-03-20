4a07cc7df7708f70b4ad6764d1776ce7e91e999d
lewis clarke <boolie205@gmail.com>
2023-03-20 17:21:16 +0100

    Add project files.

---
+++ Arma3Queue/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Arma3Queue.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arma3Queue.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand AnotherViewCommand { get; set; }
+
+        private object _currentView;
+        public HomeViewModel HomeVM { get; set; }
+        public AnotherViewModel AnotherVM { get; set; }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView= value;
+                OnPropertyChanged();
+                
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            AnotherVM = new AnotherViewModel();
+           CurrentView= HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView= HomeVM;
+            });
+            
+            AnotherViewCommand = new RelayCommand(o =>
+            {
+                CurrentView= AnotherVM;
+            });
+
+        }
+
+
+     
+    }
+}
